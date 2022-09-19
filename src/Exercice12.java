b5bda533d81cc8e31239166132ecd98cb037f626
alu10211999 <alumar1999@ieselcaminas.org>
2022-09-19 15:42:26 +0200

    Initial commit

---
+++ src/Exercice12.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercice12 {
+
+    public static void main(String[] args) {
+        int x,y;
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("Write a number:");
+        x = input.nextInt();
+        System.out.println("Write a other number:");
+        y = input.nextInt();
+
+        if (x>=y) {
+            System.out.println("The biggest number is: " + x);
+        } else {
+            System.out.println("The biggest number is: " + y);
+        }
+
+    }
+}
